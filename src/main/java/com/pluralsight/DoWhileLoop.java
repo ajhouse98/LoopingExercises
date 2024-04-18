47edc1c25d28737a01b042d7363e5948e1b5f582
ajhouse98 <ajhouse98@gmail.com>
2024-04-18 14:11:28 -0400

    Completed

---
+++ src/main/java/com/pluralsight/DoWhileLoop.java
@@ -0,0 +1,17 @@
+package com.pluralsight;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+
+        int startFrom = 0; // Init where to start
+
+        do {
+            // Enter your statement here
+            System.out.println("I love Java");
+
+            //indicate if your counting up or down
+            startFrom++;
+
+        } while (startFrom < 5); // loop 5 times
+    }
+}
---
+++ src/main/java/com/pluralsight/WhileLoop.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int startFrom = 0;
+
+        while (startFrom < 5) {
+            System.out.println("I love Java");
+            startFrom++;
+        }
+    }
+    }
